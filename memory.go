5982e11ab263f1b93b691273842a8106f98cb9fa
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-05-05 15:49:46 +0300

    added flags of end file and link list into mass

--- memory.go
+++ memory.go
@@ -12,15 +12,16 @@ type Memory struct {
 
 const memoryBlocksCount = 100
 const block_size = 1024 * 1024 // 1MB
-const freeBlockFlag = -1
+const endFile = -1
+const emptyBlock = 0
 
 func (m *Memory) findFreeSpace(needBlocks int) (blocksNumber []int64, err error) {
-	for x := 0; x < memoryBlocksCount; x++ {
+	for x := 1; x < memoryBlocksCount; x++ {
 		if needBlocks == 0 {
 			return blocksNumber, nil
 		}
 
-		if m.indexParts[x] == freeBlockFlag {
+		if m.indexParts[x] == emptyBlock {
 			blocksNumber = append(blocksNumber, int64(x))
 			needBlocks--
 		}
@@ -36,10 +37,24 @@ func (m *Memory) saveToMemory(blocksIndexNumber []int64, infoBlocks [][block_siz
 		return err
 	}
 
+	for i := 0; i < len(blocksIndexNumber)-1; i++ {
+		m.indexParts[blocksIndexNumber[i]] = blocksIndexNumber[i+1]
+	}
+	m.indexParts[blocksIndexNumber[len(blocksIndexNumber)-1]] = endFile
+
 	for i, block := range infoBlocks {
 		index := blocksIndexNumber[i]
 		m.memoryParts[index] = &block
 	}
 
 	return nil
+}
+
+func (m *Memory) loadFromMemory(blocksIndexNumber []int64) [][block_size]byte {
+	output := make([][block_size]byte, len(blocksIndexNumber))
+	for i, i2 := range blocksIndexNumber {
+		output[i] = *m.memoryParts[i2]
+	}
+
+	return output
 }
