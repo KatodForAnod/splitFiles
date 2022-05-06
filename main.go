8b2638ecac4b04be251c3742147686e79f4b363c
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-05-06 11:35:21 +0300

    main realization

--- main.go
+++ main.go
@@ -1,28 +1,15 @@
 package main
 
 import (
-	"io/ioutil"
-	"os"
 	"splitFiles/controller"
 	memory "splitFiles/memory"
+	"splitFiles/server"
 )
 
 func main() {
-	bytes1, err := ioutil.ReadFile("1.docx")
-	if err != nil {
-		return
-	}
-
 	mem := memory.Memory{}
 	c := controller.Controller{}
-	c.SetMemory(&mem)
+	c.InitController(&mem)
 
-	info, _ := c.SaveFile("s", bytes1)
-	out, _ := c.LoadFile(info)
-
-	file, err := os.Create("12.docx")
-	if err != nil {
-		return
-	}
-	file.Write(out)
+	server.StartServer(c)
 }
